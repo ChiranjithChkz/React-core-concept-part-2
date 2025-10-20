8b8945e3d1605ca42b93633adedd17579399eaca
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-10-20 16:52:42 +0600

    added the friend section with jsx

--- react-core-concept-part2/src/App.jsx
+++ react-core-concept-part2/src/App.jsx
@@ -1,9 +1,27 @@
 import Counter from './counter';
 import BatsMan from './batsMan';
 import Users from './Users';
+import friends from './friends';
 import './App.css'
+import { Suspense } from 'react';
+import Friends from './friends';
 
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users") //1//
+  .then(res => res.json());
+
+
+  const fetchFriends = async() => {  //** */
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+
+  }
+  
 function App() {
+
+
+  const friendsPromise = fetchFriends(); //** */
+
 
   function handleClick() {
     alert("I am clicked")
@@ -16,7 +34,17 @@ function App() {
     <>
 
       <h1>Vite + React</h1>
-      <Users></Users>
+      
+         
+      
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense>
+
+       <Suspense fallback={<h3>Friends are coming for treat...</h3>}> 
+        <Friends friendsPromise={friendsPromise}></Friends>
+       </Suspense>
+    
       <BatsMan></BatsMan>
       <Counter></Counter>
 
